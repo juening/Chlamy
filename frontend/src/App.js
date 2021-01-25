063f23ac8591c8c4bc3b03c02c6c397261469d5e
Jue Ning <juening@gmail.com>
2021-01-24 22:52:51 -0500

    frontend react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Hello Chlamy</h1>
+      </main>
+      <Footer />
+    </>
+
+  )
+}
+
+export default App
